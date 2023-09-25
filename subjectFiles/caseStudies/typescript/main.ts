61b9e3a79f26776974d9cba36b320530b3f8b7aa
Improvenss <gorkem_se_98@hotmail.com>
2023-09-25 10:20:30 -0400

    Added 'countdown' example.

---
+++ subjectFiles/caseStudies/typescript/main.ts
@@ -0,0 +1,12 @@
+let str: string;
+let str1: string = "Hello my friend!";
+
+
+let	num: number;
+let num2: number = 15;
+// let numBig: bigint;
+// let numBig2: bigint = 235235235235235n;
+
+console.log(str = "naber", str1);
+// console.log(num = 5, num2, numBig = 4n, numBig2);
+console.log(num = 5, num2);
