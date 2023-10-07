571adcefcef72655dfdf2614ade174ed7f5ee238
Improvenss <gorkem_se_98@hotmail.com>
2023-10-07 07:38:11 -0400

    HTML: Samples added.

--- srcs/requirements/frontend/react-app/src/login/ForwardedPage.tsx
+++ srcs/requirements/frontend/react-app/src/login/ForwardedPage.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Router, Route, Link, Routes } from 'react-router-dom';
 import Sample from './Sample';
 
 function ForwardedPage()
 {
 	return (
-	  <Router>
-		<div>
-		  <nav>
-			<ul>
-			  <li><Link to="/">Ana Sayfa</Link></li>
-			  <li><Link to="/about">Hakkımızda</Link></li>
-			</ul>
-		  </nav>
-  
-		  {/* <Route path="/" exact component={} /> */}
-		  <Route path="/about" element={<Sample/>} />
-		</div>
-	  </Router>
+		<Routes>
+			<li><Link to={"/about"}>About'a git tikla bak yenilenecek mi?</Link></li>
+			<Route path="/about" element={<Sample />} />
+		</Routes>
 	);
-  };
+}
 
 export default ForwardedPage;
