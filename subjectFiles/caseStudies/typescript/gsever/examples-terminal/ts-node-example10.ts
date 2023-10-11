2d24f033f4a00069f425e0030a235e4cad9878cf
Gorkem Sever <gsever@k2m14s08.42kocaeli.com.tr>
2023-10-11 18:18:57 +0300

    TS: function 'type' samples added.

--- subjectFiles/caseStudies/typescript/gsever/examples-terminal/ts-node-example10.ts
+++ subjectFiles/caseStudies/typescript/gsever/examples-terminal/ts-node-example10.ts
@@ -1,1 +1,24 @@
 // Burada 'Union Types' ogrenecegiz.
+// Buradaki '|' parametresinin (OR) nasil kullanildigini ogrenecegiz.
+function	unionFunction(parameterType: string | number)
+{
+	console.log("Parameter type[" + typeof(parameterType) + "]:", parameterType);
+	console.log(`Bu \`\` karakterlerinin arasina yazilirken ${parameterType}\
+	olarak 'parametre'ni cagirabilirsin.`)
+	// Boyle kullanilamaz ama...
+	// console.log(".toUpperCase() -> function'u kullanilamaz", parameterType.toUpperCase());
+	// Boyle kullanilabilinir.
+	console.log(".toUpperCase() -> function'u kullanilamaz", parameterType.toString().toUpperCase());
+}
+
+console.clear();
+unionFunction("Gorkem");
+console.log();
+unionFunction(42);
+/**
+ * Burada boyle bir tip gonderemeyiz.
+ * Cunku function'un alabilecegi parametre tiplerini;
+ * 'string' ya da 'number' olacak sekilde girdik.
+ */
+// unionFunction(true);
+
