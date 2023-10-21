431fa212e3be45cf4336e078c505920e068eeddd
Görkem Sever <gorkem_se_98@hotmail.com>
2023-10-21 04:11:31 +0300

    APP: Font first load(preload) font in process...

--- srcs/requirements/frontend/react-app/src/App.tsx
+++ srcs/requirements/frontend/react-app/src/App.tsx
@@ -1,42 +1,36 @@
 import React, { useEffect, useState } from "react";
 import { Routes, Route, Link } from 'react-router-dom';
+import { Helmet } from "react-helmet"; // Bu React JSX'in icerisinde <link> calistirabilmek icin.
 import './App.css';
 import HomePage from "./main/HomePage"; // '/' root dizini.
-import Countdown from "./login/Countdown";
 import Address from "./client/Address";
 import Loading from "./login/Loading";
-
-// function	useEffect()
-// {
-// 	// const	loadingScreen = document.getElementsByClassName('loading-screen');
-
-// 	window.addEventListener('load', () => {
-// 		const	loadingScreen = document.getElementById('loading-screen')
-
-// 		if (loadingScreen)
-// 			loadingScreen.
-// 	});
-// }
-
-
 
 function	App()
 {
 	const	[loading, setLoading] = useState(true);
 
+	const handleLoad = (e: Event) => {
+		setLoading(false);
+	};
+
 	useEffect(() =>
 	{
-		setTimeout(() => setLoading(false), 1000)
+		window.addEventListener('load', handleLoad);
+		return () => {
+			window.removeEventListener('load', handleLoad);
+		};
 	}, []);
 
 	if (loading)
 	{
-		// return (loading ? <Loading/> : <App/>);
 		return (<Loading/>);
 	}
 
 	return (
 		<div id="id-app">
+			<Helmet>
+			</Helmet>
 			<header>
 				<ul>
 					<li><Link to="/">Anasayfa</Link></li>
@@ -47,8 +41,6 @@ function	App()
 			<Routes>
 				<Route path="/" element={<HomePage/>} />
 				<Route path="/address" element={<Address/>} />
-				{/* <Route path="/login" element={</>} /> */}
-				{/* <Route path="*" element={<NotLoaded/>} /> */}
 			</Routes>
 		</div>
 	);
@@ -56,30 +48,88 @@ function	App()
 
 export default	App;
 
-//------------------------------------------------
+//----------------------------------------------
 
-// import React, { useEffect } from 'react';
-// import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
-// import Main from './main/Main.tsx';
+// import React, { useEffect, useState } from "react";
+// import { Routes, Route, Link } from 'react-router-dom';
+// import './App.css';
+// import HomePage from "./main/HomePage"; // '/' root dizini.
+// import Countdown from "./login/Countdown";
+// import Address from "./client/Address";
+// import Loading from "./login/Loading";
 
+// // function	useEffect()
+// // {
+// // 	// const	loadingScreen = document.getElementsByClassName('loading-screen');
 
-// const App = () => {
-// useEffect(() => {
-// 	// Sayfa yüklendiğinde yükleme ekranını kaldır
-// 	window.addEventListener('load', () => {
-// 	const loadingScreen = document.getElementById('loading-screen');
-// 	if (loadingScreen)
-// 		loadingScreen.style.display = 'none';
-// 	});
-// }, []);
+// // 	window.addEventListener('load', () => {
+// // 		const	loadingScreen = document.getElementById('loading-screen')
 
-// return (
-// 	<Router>
-// 	<div id="loading-screen">Sayfa şu anda yükleniyor. Lütfen bekleyiniz.</div>
-// 		<Route path="/login" component={Main} />
-// 		<Redirect to="/login" />
-// 	</Router>
-// );
+// // 		if (loadingScreen)
+// // 			loadingScreen.
+// // 	});
+// // }
+
+// function	App()
+// {
+// 	const	[loading, setLoading] = useState(true);
+
+// 	useEffect(() =>
+// 	{
+// 		setTimeout(() => setLoading(false), 500)
+// 	}, []);
+
+// 	if (loading)
+// 	{
+// 		// return (loading ? <Loading/> : <App/>);
+// 		return (<Loading/>);
+// 	}
+
+// 	return (
+// 		<div id="id-app">
+// 			<header>
+// 				<ul>
+// 					<li><Link to="/">Anasayfa</Link></li>
+// 					<li><Link to="/login">Login</Link></li>
+// 					<li><Link to="/address">Address</Link></li>
+// 				</ul>
+// 			</header>
+// 			<Routes>
+// 				<Route path="/" element={<HomePage/>} />
+// 				<Route path="/address" element={<Address/>} />
+// 				{/* <Route path="/login" element={</>} /> */}
+// 				{/* <Route path="*" element={<NotLoaded/>} /> */}
+// 			</Routes>
+// 		</div>
+// 	);
 // };
 
-// export default App;
+// export default	App;
+
+// //------------------------------------------------
+
+// // import React, { useEffect } from 'react';
+// // import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+// // import Main from './main/Main.tsx';
+
+
+// // const App = () => {
+// // useEffect(() => {
+// // 	// Sayfa yüklendiğinde yükleme ekranını kaldır
+// // 	window.addEventListener('load', () => {
+// // 	const loadingScreen = document.getElementById('loading-screen');
+// // 	if (loadingScreen)
+// // 		loadingScreen.style.display = 'none';
+// // 	});
+// // }, []);
+
+// // return (
+// // 	<Router>
+// // 	<div id="loading-screen">Sayfa şu anda yükleniyor. Lütfen bekleyiniz.</div>
+// // 		<Route path="/login" component={Main} />
+// // 		<Redirect to="/login" />
+// // 	</Router>
+// // );
+// // };
+
+// // export default App;
--- srcs/requirements/frontend/react-app/src/client/Address.tsx
+++ srcs/requirements/frontend/react-app/src/client/Address.tsx
@@ -1,21 +1,19 @@
-import React, {useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function	Address()
 {
-	const	[ipAddress, setIpAddress] = useState("");
-	const	[network, setNetwork] = useState("");
-	const	[ipApiData, setipApiData] = useState("");
+	// const	[ipAddress, setIpAddress] = useState("Loading...");
+	// const	[network, setNetwork] = useState("Loading...");
+	// const	[ipApiData, setIpApiData] = useState("Loading...");
+	let		[ipAddress, setIpAddress] = useState("");
+	let		[network, setNetwork] = useState("");
+	let		[ipApiData, setIpApiData] = useState("");
+	// const	[ipAddress, setIpAddress] = useState("");
+	// const	[network, setNetwork] = useState("");
+	// const	[ipApiData, setIpApiData] = useState("");
+	const	[loading, setLoading] = useState(true);
+	const	[error, setError] = useState(null);
 
-	// useEffect(() =>
-	// {
-	// 	fetch("https://ipapi.co/json")
-	// 		.then((response) => response.json())
-	// 		.then((data) =>
-	// 		{
-	// 			setIpAddress(data.ip);
-	// 			setNetwork(data.network);
-	// 		});
-	// }, []);
 	useEffect(() =>
 	{
 		fetch("https://ipapi.co/json")
@@ -24,18 +22,32 @@ function	Address()
 			{
 				setIpAddress(data.ip);
 				setNetwork(data.network);
-				setipApiData(data);
-			});
+				setIpApiData(data);
+			})
+			.catch((error) => setError(error))
+			.finally(() => setLoading(false));
 	}, []);
 
+	if (loading)
+	{
+		ipAddress = "Loading...";
+		network = "Loading...";
+		ipApiData = "Loading...";
+		// return (<p>"Loading..."</p>);
+	}
+	if (error)
+	{
+		ipAddress = "Error!";
+		network = "Error!";
+		ipApiData = "Error!";
+		// return (<p>"Error!"</p>);
+	}
 
 	return (
 		<div className="ip-address">
-			{/* <p className="ip">Your ip will show here...</p>
-			<p className="network">Your network address will show here...</p> */}
-			<p>Your public IP Adress: {ipAddress}</p>
-			<p>Your IP Network: {network}</p>
-			<p>All information; {JSON.stringify(ipApiData, null, 2)}</p>
+		<p id="id-public-ip">Your public IP Address: {ipAddress}</p>
+		<p>Your IP Network: {network}</p>
+		<p>All information; {JSON.stringify(ipApiData, null, 2)}</p>
 		</div>
 	);
 };
