50f830c72bcc168c496650e285473631eb2c95d4
Gorkem Sever <gsever@k2m14s08.42kocaeli.com.tr>
2023-10-30 20:40:37 +0300

    DB: 'create' & 'add' code and commit samples added.

--- srcs/requirements/backend/nest-js/src/app.module.ts
+++ srcs/requirements/backend/nest-js/src/app.module.ts
@@ -2,10 +2,29 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoginModule } from './login/login.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersController } from './users/users.controller';
+import { UsersService } from './users/users.service';
+import { UsersModule } from './users/users.module';
+import 'dotenv/config'; // <- this line is the important
 
 @Module({
-	imports: [LoginModule],
-	controllers: [AppController],
-	providers: [AppService],
+	imports: [
+		LoginModule,
+		TypeOrmModule.forRoot({
+			type: 'postgres',
+			port: parseInt(process.env.POSTGRES_PORT),
+			host: process.env.POSTGRES_HOST,
+			username: process.env.POSTGRES_USER,
+			password: process.env.POSTGRES_PASSWORD,
+			database: process.env.POSTGRES_DB,
+			entities: [],
+			synchronize: true, //false for production, else destroy/recreate data in the db
+		}),
+		UsersModule,
+	],
+	controllers: [AppController, UsersController],
+	providers: [AppService, UsersService],
 })
 export class AppModule {}
+
--- srcs/requirements/backend/nest-js/src/app.service.ts
+++ srcs/requirements/backend/nest-js/src/app.service.ts
@@ -4,6 +4,5 @@ import { Injectable } from '@nestjs/common';
 export class AppService {
 	getHello(): string {
 		return ('Hello World! from backend/nest-js/src/app.service.ts');
-		// return ('asfdasdfas');
 	}
 }
